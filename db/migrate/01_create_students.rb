0abefed7ab9888e071f3baa6f62fabf25f48ea72
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-21 04:02:21 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,5 +3,7 @@ class CreateStudents < ActiveRecord::Migration[5.2]
     #we are defining one of the changes we are going to make
     create_table :students do |s|
       s.string :name
-  
-end
+    end 
+  end
+end 
+
