94c4fb5f006a13c066879bab903a079d92e23c3b
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-21 04:06:48 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -3,3 +3,6 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
     add_column
   end 
 end 
+
+
+#Let's add a :grade column and a :birthdate column. The :grade column type should be integer and the :birthdate column type should be string.
